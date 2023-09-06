722a13509c203f8649ed72a1efc8fcadcac5c98a
Dar Malovani <dar.malovani@wilcohq.com>
2023-09-06 13:31:35 +0300

    right solution

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -51,7 +51,7 @@ const App = (props) => {
   useEffect(() => {
     if (token) {
       if (isTokenExpired(token)) {
-        navigate(`/loginwrong`);
+        navigate(`/login`);
       }
       agent.setToken(token);
     }
